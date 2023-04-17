c3f836270d373502963afc92222c08447b1fe8d0
Hanna Kulai <hkulai@inspirebrands.com>
2023-04-17 18:41:47 +0300

    [task-5]: import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://0zd2ie8wva.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://8qhgit03g3.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
