903312cb336adead7aaebca065ad5bd9eb321d18
Hanna Kulai <hkulai@inspirebrands.com>
2023-05-22 23:16:16 +0300

    [TASK-9]: (Docker and AWS Elastic Beanstalk)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://8qhgit03g3.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://annakulai-cart-api-dev.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
